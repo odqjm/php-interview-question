11e5e64eb7a1209136c4ef62cfe8606aa6fcab21
eps <epsmail@126.com>
2019-02-13 09:12:51 +0800

    update

--- 12-检查数据是否为整数.php
+++ 12-检查数据是否为整数.php
@@ -5,15 +5,88 @@
  * DateTime: 12/11/2018 1:04 PM
  */
 
-// 检查十六进制
-function isHex($number) {
+/**
+ * checkDecimal
+ * @Author: eps
+ * @DateTime DATETIME
+ * @param $value
+ * @return bool|string
+ */
+
+/**
+ * 判断数据的进制
+ * @Author: eps
+ * @DateTime DATETIME
+ * @param $value
+ * @return bool|string
+ */
+function checkDecimal($value)
+{
+    // 判断十六进制, 要求是 0x 开头
+    if (strstr($value, '0x')) {
+        return 'hex';
+    }
+
+    // 判断八进制, 要求是 0 开头
+    if (strstr($value, '0')) {
+        return 'octal';
+    }
+
+    // 判断是否是一个整数
+    if (intval($value) != $value) {
+        return false;
+    }
+}
+
+/**
+ * 判断八进制
+ * @Author: eps
+ * @DateTime DATETIME
+ * @param $number
+ * @return bool
+ */
+function isOctal($number)
+{
+    $number = substr($number, 1);
+    return $number == '0' . decoct(octdec($number));
+}
+
+/**
+ * 判断十六进制
+ * @Author: eps
+ * @DateTime DATETIME
+ * @param $number
+ * @return bool
+ */
+function isHex($number)
+{
+    $number = substr($number, 2);
     return $number === dechex(hexdec($number));
 }
 
-// 检查是否是整数
-function isInteger($number) {
-    if (!is_numeric($number)) {
+/**
+ * 检查整数
+ * @Author: eps
+ * @DateTime DATETIME
+ * @param $number
+ * @return bool
+ */
+function isInteger($number)
+{
+    $number = trim(strtolower($number));
+
+    // 判断是什么进制
+    $from = checkDecimal($number);
+
+    if (!$from) {
         return false;
+    }
+
+    if ($from == 'hex' && isHex($number)) {
+        return true;
+    }
+    if ($from == 'octal' && isOctal($number)) {
+        return true;
     }
     if (filter_var($number, FILTER_VALIDATE_INT) !== false) {
         return true;
@@ -24,16 +97,14 @@ function isInteger($number) {
     return false;
 }
 
+# 现只适用于 正整数的判断
+/*
+ * TODO 负数的判断
+ */
 
-$post = [
-    '',
-    '0',
-    '-1',
-    '9',
-    '7',
-    'F',
-];
-$value = $post[mt_rand(0, count($post) - 1)];
+$post = ['0', '1', '0XA', '0XB', 'OXB'];
 
-echo $value . PHP_EOL;
-var_dump(isInteger($value));
+foreach ($post as $number) {
+    echo $number . PHP_EOL;
+    var_dump(isInteger($number));
+}
